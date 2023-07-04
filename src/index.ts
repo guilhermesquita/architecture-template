2e689499f29315f137801fc5bbf045e7e4afde6b
guilhermer_mesquita@hotmail.com <guilhermer_mesquita@hotmail.com>
2023-07-04 17:54:00 -0400

    chore: config express and insert cors

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world')
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3003, ()=>console.log('listening on http://localhost/3003'))
