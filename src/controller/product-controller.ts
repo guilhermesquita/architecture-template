4d0fe6fa91d32b54000153f4904ccfcf899eb1e1
guilhermer_mesquita@hotmail.com <guilhermer_mesquita@hotmail.com>
2023-07-05 01:25:18 -0400

    feat: implementation path params

--- src/controller/product-controller.ts
+++ src/controller/product-controller.ts
@@ -4,12 +4,20 @@ import { db } from '../database/baseDatabase'
 export class ProductController {
     constructor() {}
 
-    public getProducts = async (_req: Request, res: Response) => {
+    public getProducts = async (req: Request, res: Response) => {
         try {
-
-            const output = await db("products").select()
-            res.status(200).send(output)
-
+            const q = req.query.q
+    
+            let output;
+    
+            if (q){
+                output = await db("products").select().where("name", "LIKE", `%${q}%`)
+            } else {
+                output = await db("products").select()
+            }
+    
+            res.status(200).send(output);
+    
         } catch (error) {
             if (error instanceof Error) {
                 res.status(500).send(error.message)
